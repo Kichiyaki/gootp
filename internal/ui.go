f864038628f96cf9e126082b34d83bf974b70be3
Dawid Wysokiński <contact@dwysokinski.me>
2022-05-19 16:11:53 +0200

    feat: add simple TUI

--- internal/ui.go
+++ internal/ui.go
@@ -1,15 +1,20 @@
 package internal
 
 import (
+	"time"
+
 	"github.com/charmbracelet/bubbles/list"
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/lipgloss"
 )
 
+const (
+	tickDuration = 1 * time.Second
+)
+
 var docStyle = lipgloss.NewStyle().Margin(1, 2)
 
 type item struct {
-	entry       Entry
 	title       string
 	description string
 }
@@ -24,22 +29,31 @@ func (i item) FilterValue() string {
 	return i.title
 }
 
+type refreshOTPsMsg struct {
+	t time.Time
+}
+
 type UI struct {
-	list list.Model
+	list    list.Model
+	entries []Entry
 }
 
 func NewUI(entries []Entry) UI {
-	ui := UI{list: list.New(entriesToItems(entries), list.NewDefaultDelegate(), 0, 0)}
+	ui := UI{
+		list:    list.New(entriesToItems(entries, time.Now()), list.NewDefaultDelegate(), 0, 0),
+		entries: entries,
+	}
 	ui.list.Title = "GoOTP"
 	return ui
 }
 
 func (ui UI) Init() tea.Cmd {
-	return nil
+	return ui.tick()
 }
 
-func (ui UI) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
-	switch msg := msg.(type) {
+func (ui UI) Update(teaMsg tea.Msg) (tea.Model, tea.Cmd) {
+	var cmd tea.Cmd
+	switch msg := teaMsg.(type) {
 	case tea.KeyMsg:
 		if msg.String() == "ctrl+c" {
 			return ui, tea.Quit
@@ -47,25 +61,44 @@ func (ui UI) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	case tea.WindowSizeMsg:
 		h, v := docStyle.GetFrameSize()
 		ui.list.SetSize(msg.Width-h, msg.Height-v)
+	case refreshOTPsMsg:
+		ui.list.SetItems(entriesToItems(ui.entries, msg.t))
+		cmd = ui.tick()
 	}
 
-	var cmd tea.Cmd
-	ui.list, cmd = ui.list.Update(msg)
-	return ui, cmd
+	var listCmd tea.Cmd
+	ui.list, listCmd = ui.list.Update(teaMsg)
+	return ui, tea.Batch(cmd, listCmd)
 }
 
 func (ui UI) View() string {
 	return docStyle.Render(ui.list.View())
 }
 
-func entriesToItems(entries []Entry) []list.Item {
+func (ui UI) tick() tea.Cmd {
+	return tea.Tick(tickDuration, func(t time.Time) tea.Msg {
+		return refreshOTPsMsg{t: t}
+	})
+}
+
+func entriesToItems(entries []Entry, t time.Time) []list.Item {
 	items := make([]list.Item, len(entries))
 	for i, e := range entries {
 		title := e.Label
 		if e.Issuer != "" {
 			title = e.Issuer + " - " + e.Label
 		}
-		items[i] = item{entry: e, title: title}
+
+		otp, err := GenerateOTP(e, t)
+		description := otp
+		if err != nil {
+			description = err.Error()
+		}
+
+		items[i] = item{
+			title:       title,
+			description: description,
+		}
 	}
 	return items
 }
