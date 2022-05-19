a19ca86c0e1702272902882a41991c9d7be96352
Dawid Wysokiński <contact@dwysokinski.me>
2022-05-19 16:36:28 +0200

    feat: add simple TUI

--- main.go
+++ main.go
@@ -55,18 +55,7 @@ func newApp() (*cli.App, error) {
 				return fmt.Errorf("something went wrong while decrypting file: %w", err)
 			}
 
-			//for _, entry := range entries {
-			//	otp, err := internal.GenerateOTP(entry)
-			//	if err != nil {
-			//		log.Printf("%s - %s: %s", entry.Issuer, entry.Label, err)
-			//		continue
-			//	}
-			//
-			//	log.Printf("%s - %s: %s", entry.Issuer, entry.Label, otp)
-			//}
-
 			p := tea.NewProgram(internal.NewUI(entries), tea.WithAltScreen())
-
 			if err := p.Start(); err != nil {
 				return fmt.Errorf("p.Start: %w", err)
 			}
